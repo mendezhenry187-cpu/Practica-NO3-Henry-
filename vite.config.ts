2ba4b313ff412043d14317cd887dda8d1074c78b
mendezhenry187-cpu <mendezhenry187@gmail.com>
2026-09-22 18:04:38 -0600

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -10,9 +10,10 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/Practica-NO3-Henry-/',
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
 })
